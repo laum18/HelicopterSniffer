8267b491cff7ba25961b9a3bb2d74cac0f172e4f
laum18 <laum18@up.edu>
2018-04-23 18:31:09 -0700

    added labels

--- helicopter_sniffer.py
+++ helicopter_sniffer.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QPlainTextEdit, QVBoxLayout#, QtGui
 
 
 class App(QMainWindow):
@@ -16,6 +16,24 @@ class App(QMainWindow):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        
+#        # drop down menu
+#        self.styleChoice = QtGui.QLabel("Windows Vista", self)
+##        self.styleChoice.move(50,150)
+#
+#        comboBox = QtGui.QComboBox(self)
+#        comboBox.addItem("motif")
+#        comboBox.addItem("Windows")
+#        comboBox.addItem("cde")
+#        comboBox.addItem("Plastique")
+#        comboBox.addItem("Cleanlooks")
+#        comboBox.addItem("windowsvista")
+#        comboBox.move(50, 250)
+#
+#        comboBox.activated[str].connect(self.style_choice)
+#
+#        self.styleChoice.move(50,150)
+#        comboBox.activated[str].connect(self.style_choice)
 
         # basic menu
         mainMenu = self.menuBar()
@@ -25,7 +43,27 @@ class App(QMainWindow):
 
         # status bar
         self.statusBar().showMessage('Message in statusbar.')
+        
+        # labels
+        trafficLabel = QLabel('Traffic', self)
+        trafficLabel.move(100,50)
+        childLabel = QLabel('Child/Device', self)
+        childLabel.move(425, 50)
+        websitesLabel = QLabel('Websites Visited', self)
+        websitesLabel.move(75, 250)
+        
+        # text section
+#        self.b = QPlainTextEdit(self)
+#        self.b.insertPlainText("You can write text here.\n")
+#        self.b.move(10,10)
+#        self.b.resize(400,200)
+
+
         self.show()
+
+    def style_choice(self, text):
+        self.styleChoice.setText(text)
+        QtGui.QApplication.setStyle(QtGui.QStyleFactory.create(text))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
