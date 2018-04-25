fded6c084309a7a41fe1629874ef066eb5b2450c
808Chase <deslauri19@up.edu>
2018-04-24 22:59:54 -0700

    yay! it works

--- 2testpcap.c
+++ 2testpcap.c
@@ -11,13 +11,13 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <time.h>
 
 
 void my_packet_handler(u_char* args, const struct pcap_pkthdr* packet_header, const u_char* packet_body);
 void get_whois(char *ip , char **data);
 int whois_query(char *server , char *query , char **response);
 int hostname_to_ip(char * hostname , char* ip);
-
 
 
 int main(int argc, char **argv)
@@ -97,13 +97,13 @@ void my_packet_handler(u_char* args, const struct pcap_pkthdr* packet_header, co
     struct ether_header * eth_header;
     eth_header = (struct ether_header *) packet;
     if(ntohs(eth_header->ether_type) != ETHERTYPE_IP) {
-        printf("Not and IP packet. Skipping...\n\n");
+        //printf("Not and IP packet. Skipping...\n\n");
         return;
     }
     
     
-    printf("Packet capture length: %d\n", packet_header->caplen);
-    printf("Packet total length: %d\n", packet_header->len);
+    //printf("Packet capture length: %d\n", packet_header->caplen);
+    //printf("Packet total length: %d\n", packet_header->len);
     
     
     const u_char* ip_header;
@@ -120,27 +120,78 @@ void my_packet_handler(u_char* args, const struct pcap_pkthdr* packet_header, co
     ip_header = packet + ethernet_header_length;
     ip_header_length = ((*ip_header) & 0x0F);
     ip_header_length = ip_header_length * 4;
-    printf("IP header length (IHL) in bytes: %d\n", ip_header_length);
+    //printf("IP header length (IHL) in bytes: %d\n", ip_header_length);
     
     
     u_char protocol = *(ip_header + 9);
     if(protocol != IPPROTO_TCP) {
-        printf("Not a TCP packet. Skipping...\n\n");
+        //printf("Not a TCP packet. Skipping...\n\n");
         return;
     }
     
     tcp_header = packet + ethernet_header_length + ip_header_length;
     tcp_header_length = ((*(tcp_header + 12)) & 0xF0) >> 4;
     tcp_header_length = tcp_header_length * 4;
-    printf("TCP header length in bytes: %d\n", tcp_header_length);
+    //printf("TCP header length in bytes: %d\n", tcp_header_length);
     
     int total_header_size = ethernet_header_length + ip_header_length + tcp_header_length;
     payload_length = packet_header->caplen - total_header_size;
-    printf("Payload size: %d bytes\n", payload_length);
+    //printf("Payload size: %d bytes\n", payload_length);
     payload = packet + total_header_size;
-    printf("Memory address where payload begins: %p\n\n", payload);
+    //printf("Memory address where payload begins: %p\n\n", payload);
     
-    printf("\nPAYLOAD:\n%s\n\n", payload);
+    
+    char begin[7];
+    //memcpy(begin,payload, 5);
+    int i;
+    for(i = 0; i < 6; i++)
+        begin[i] = payload[i];
+    begin[6] = '\0';
+    //printf("Got: %s\n", begin);
+    if(strcmp(begin, "GET / ") == 0) {
+        char host[200];
+        
+        for(i = 0; i < strlen(host); i++)
+            host[i] = '0';
+        
+        for(i = 0; i < 200; i++) {
+            host[i] = payload[i+21];
+            //printf("begin[i]: %c\n", host[i]);
+            if(payload[i+22] == '.' && payload[i+23] == 'c' && payload[i+24] == 'o' && payload[i+25] == 'm') {
+                host[i+1] = '.';
+                host[i+2] = 'c';
+                host[i+3] = 'o';
+                host[i+4] = 'm';
+                break;
+            }
+        }
+        //printf("host[1]: %c\n", host[1]);
+        if(host[1] != 'w') {
+            printf("host: %s\n", host);
+            FILE * websiteFile = fopen("websites.txt", "a");
+            for(i = 0; i < strlen(host); i++)
+                if(host[i] != '0')
+                    fputc(host[i], websiteFile);
+                else
+                    break;
+            
+            time_t rawtime;
+            struct tm * timeinfo;
+            time(&rawtime);
+            timeinfo = localtime(&rawtime);
+            fputs(", ", websiteFile);
+            fputs(asctime(timeinfo), websiteFile);
+            fputs("\r\n", websiteFile);
+            //fputs(host, websiteFile);
+            fclose(websiteFile);
+        }
+        
+               
+        //printf("\nPAYLOAD:\n%s\n\n", payload);
+        
+    }
+    
+    
     
     const struct ip* ip;
     ip = (struct ip*)(packet + ethernet_header_length);
@@ -148,34 +199,16 @@ void my_packet_handler(u_char* args, const struct pcap_pkthdr* packet_header, co
     int len = ntohs(ip->ip_len);
     
     if(ip->ip_v != 4) {
-        printf("Unknown version %d\n", ip->ip_v);
+        //printf("Unknown version %d\n", ip->ip_v);
         return;
     }
     
     if(ip->ip_hl < 5) {
-        printf("bad header length %d\n", ip->ip_hl);
+        //printf("bad header length %d\n", ip->ip_hl);
         return;
     }
     
-    printf("IP source: %s\n", inet_ntoa(ip->ip_src));
-    printf("IP dest: %s\n\n\n", inet_ntoa(ip->ip_dst));
     
-    
-    
-    
-    /*
-    //print contents
-    if(payload_length > 0) {
-        const u_char * temp_pointer = payload;
-        int byte_count = 0;
-        while(byte_count++ < payload_length) {
-            printf("%c", *temp_pointer);
-            temp_pointer++;
-        }
-        printf("\n");
-    }
-    */
- 
     return;
 }
 
