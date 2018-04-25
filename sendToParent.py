06bfd3a025074032f2813e992869afb2f98099fe
NalaniKai <megan.nalani@gmail.com>
2018-04-25 00:29:51 -0700

    update

--- sendToParent.py
+++ sendToParent.py
@@ -1,6 +1,6 @@
 import socket 
+import time
 
-print("meow")
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(("", 30000))
 sock.listen(1)
@@ -8,11 +8,13 @@ print("waiting for connection")
 (s, address) = sock.accept()
 print("accepted connection")
 
-with open("websites.txt", 'r') as f:
-    content = f.read(1024)
-    while content:
-        s.send(content.encode())
+while True:        
+    with open("websites.txt", 'r') as f:
         content = f.read(1024)
+        while content:
+            s.send(content.encode())
+            content = f.read(1024)    
+    time.sleep(40)
+    
 s.close()
 sock.close()
-    
